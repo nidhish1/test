5cfcccf901d731dcb3eeed488d1df3e91cdb5271
nidhish1 <vortex@localhost.localdomain>
2020-02-14 02:56:38 +0530

    sdf

---
+++ internal/test.go
@@ -0,0 +1,9 @@
+package test
+
+import (
+	"fmt"
+)
+
+func Foo () {
+	fmt.Println("sdf")
+}
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nidhish1/test/test"
+)
+
+
+func main () {
+	fmt.Println("sfs")
+	test.Foo()
+}
+
+
